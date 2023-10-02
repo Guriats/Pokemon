408675093221570340d3d562c593e54add087552
UsuarioM <espacioguriats@gmail.com>
2023-10-02 13:57:05 +0200

    pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
-
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import axios, { Axios } from "axios";
 
 const App = () => {
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    number: "",
+    species: "",
+    image: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    speed: "",
+    type: "",
+  });
 
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (res) => {
+        setPokemon({
+          name: pokemonName,
+          number: res.data.id,
+          species: res.data.species.name,
+          image: res.data.sprites.front_default,
+          hp: res.data.stats[0].base_stat,
+          attack: res.data.stats[1].base_stat,
+          defense: res.data.stats[2].base_stat,
+          speed: res.data.stats[5].base_stat,
+          type: res.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
+      }
+    );
+  };
 
   return (
-    <>
-     
-    </>
-  )
-}
+    <div className="App">
+      <div className="TitleSection">
+        <h1>Pokédex</h1>
+        <input
+          type="text"
+          onChange={(ev) => setPokemonName(event.target.value)}
+        />
+        <button onClick={searchPokemon}>Search Pokémon</button>
+      </div>
+      <div className="DisplaySection">
+        {!pokemonChosen ? (
+          <h1> Please choose a Pokémon </h1>
+        ) : (
+          <h1>{pokemon.name}</h1>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
